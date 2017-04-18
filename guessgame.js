3dd2f314dcc8028f502ff6b9a03a1375e9ad4731
louiscocks <99043889@mydavinci.nl>
2017-04-18 13:12:30 +0200

    update.JsGame. Made some changes to Bootstrap and added a real-time javascript clock.

--- guessgame.js
+++ guessgame.js
@@ -1,34 +1,54 @@
 var guessme=Math.round(Math.random()*(99)+1);
 var speech='Guess my number (from 1 to 100)';
 
+alert("Guess the number from 1 to 100");
+
 function process(mystery) {
-var guess=document.forms.guessquiz.guess.value;
-var speech='"'+guess+ '" does not make sense to me.';
-document.forms.guessquiz.guess.value='';
 
-if (guess==mystery)
-{
-document.forms.guessquiz.prompt.value='Congratulations! '+mystery+' is correct!';
-alert ('Well done - the mystery number is '+mystery+'! \n\nPress this button to reload the page for another game.');
-speech='';
-document.location=document.location;
+	var guess=document.forms.guessquiz.guess.value;
+	var speech=' "'+guess+ '" Does not make sense to me. ';
+	document.forms.guessquiz.guess.value='';
+
+		if (guess==mystery)
+		{
+		document.forms.guessquiz.prompt.value='Congratulations! '+mystery+' is correct!';
+		alert ('Well done - the number is '+mystery+'! \n\nPress this button to reload the page for another game.');
+		speech='';
+		document.location=document.location;
+		}
+
+		if (mystery<guess)
+		{
+		speech='Less than '+ guess;
+		}
+
+		if (mystery>guess)
+		{
+		speech='Greater than '+ guess;
+		}
+
+		if (guess=='')
+		{
+		speech='You didn\'t guess anything!'
+		}
+
+	document.forms.guessquiz.prompt.value=speech; 
+	document.forms.guessquiz.guess.focus();
+
 }
 
-if (mystery<guess)
-{
-speech='Less than '+ guess;
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('box').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
 }
-
-if (mystery>guess)
-{
-speech='Greater than '+ guess;
-}
-
-if (guess=='')
-{
-speech='You didn\'t guess anything!'
-}
-
-document.forms.guessquiz.prompt.value=speech; document.forms.guessquiz.guess.focus();
-
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
 }
